4f86e87ed9d2c5a0cca2b316ffab0fda7cc38502
Evgeniy <e.vasiliev.86@mail.ru>
2023-04-09 00:18:52 +0300

    file edit

--- controller.py
+++ controller.py
@@ -1,17 +1,19 @@
 import read_notes as rn
 import add_notes as an
 import delete_note as dn
-
+import edit_notes as en
 def start():
     while True:
         print("Введите номер действия:")
         print("1. Создать заметку")
         print("2. Просмотр всех заметок")
         print("3. Удаление заметки")
+        print("4. Редактирование заметок")
         num = input()
         match num:
             case "1":
                 an.add_notes()
+                print("Заметка создана")
             case "2":
                 notes = rn.read_notes()
                 for note in notes:
@@ -20,3 +22,6 @@ def start():
             case "3":
                 dn.delete_note()
                 print("Заметка удалена")
+            case "4":
+                en.edit_note()
+                print("Заметка изменена")
