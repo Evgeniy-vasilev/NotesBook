3a4aef0d7973610ca2c1ccc77eae8e450e93f907
Evgeniy <e.vasiliev.86@mail.ru>
2023-04-09 11:33:57 +0300

    fix filter & gitignore

--- filter.py
+++ filter.py
@@ -1,12 +1,12 @@
 import read_notes as rn
 from datetime import datetime
 
+
 # Фильтр по дате
 def filter():
-    from_date = datetime.strptime(input("Введите дату/время в формате ГГГГ-ММ-ДД ЧЧ:ММ (например, 2022-01-01 12:00): "),
-                                  "%Y-%m-%d %H:%M")
+    date = datetime.strptime(input("Введите дату/время в формате ГГГГ-ММ-ДД ЧЧ:ММ:CC "), "%Y-%m-%d %H:%M:%S")
     notes = rn.read_notes()
-    notes_filter = [note for note in notes if datetime.strptime(note["created"], "%Y-%m-%d %H:%M:%S") >=
-                    from_date or note["updated"] and datetime.strptime(note["updated"], "%Y-%m-%d %H:%M:%S") >=
-                    from_date]
+
+    notes_filter = [note for note in notes if datetime.strptime(note["created"], "%Y-%m-%d %H:%M:%S") == date or
+                    note["updated"] and datetime.strptime(note["updated"], "%Y-%m-%d %H:%M:%S") == date]
     return notes_filter
